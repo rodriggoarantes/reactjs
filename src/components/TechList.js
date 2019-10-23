b99799b84b27098199029a4f0ace29e469581e95
rodrigo <rodrigo.arantes@totvs.com.br>
2019-10-23 19:31:24 -0300

    Primeiro componente React com estado

---
+++ src/components/TechList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class TechList extends Component {
+    state = {
+        newTech: '',
+        techs: [
+            'Node.js',
+            'React.js',
+            'React Native'
+        ]
+    };
+
+    handleInputChange = (e) => {
+        this.setState({newTech: e.target.value})
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({ techs: [...this.state.techs, this.state.newTech], newTech: ''});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h1>{this.state.newTech}</h1>
+                <ul>
+                    {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+                </ul>
+                <input 
+                    type='text' 
+                    onChange={this.handleInputChange} 
+                    value={this.state.newTech}/>
+                <button type='submit'>Enviar</button>
+            </form>
+        );
+    }
+}
+
+export default TechList;
