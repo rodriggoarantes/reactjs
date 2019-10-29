3d826046bd55d10856a37d7b19de28930480cb6a
rodrigo <rodrigo.arantes@totvs.com.br>
2019-10-29 13:47:07 -0300

    Estilizando componentes

---
+++ src/styles/global.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0px;
+    padding: 0px;
+    outline: 0px;
+    box-sizing: border-box;
+  }
+
+  html, body, div#root {
+    min-height: 100%;
+  }
+
+  body {
+    background: #7159c1;
+    -webkit-font-smoothing: antialiased !important;
+    -moz-osx-font-smoothing: grayscale !important;
+  }
+
+  body, input, button {
+    color: #222;
+    font-size: 14px;
+    font-family: Arial, Helvetica, sans-serif, "Roboto";
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
