b999c46529c656111f949c8294a9cbd843a071ab
rodrigo <rodrigo.arantes@totvs.com.br>
2019-10-30 14:01:09 -0300

    salvando repositorios existentes no github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { Title } from './styles';
+import api from '../../services/api';
+import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return <Title>Hello React</Title>;
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+  }
+
+  handleInputChange = event => {
+    this.setState({ newRepo: event.target.value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="white" size={14} />
+            ) : (
+              <FaPlus color="white" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
-
-export default Main;
