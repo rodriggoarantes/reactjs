2fadb0d849e5faf386839cf116c9d78e1db03892
rodrigo <rodrigo.arantes@totvs.com.br>
2019-11-08 14:03:12 -0300

    Adicionando Redux para adicionar produtos ao carrinho

--- reactshoes/src/components/Header/index.js
+++ reactshoes/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -7,10 +8,9 @@ import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-export default class Header extends Component {
+class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
   }
 
   render() {
@@ -23,7 +23,7 @@ export default class Header extends Component {
         <Cart to="/cart">
           <div>
             <strong>Meu carrinho</strong>
-            <span>3 itens</span>
+            <span>{this.props.cartSize} itens</span>
           </div>
           <MdShoppingBasket size={36} color="#FFF" />
         </Cart>
@@ -31,3 +31,7 @@ export default class Header extends Component {
     );
   }
 }
+
+export default connect(state => ({
+  cartSize: state.cart && state.cart.length,
+}))(Header);
--- reactshoes/src/store/modules/cart.js
+++ reactshoes/src/store/modules/cart.js
@@ -1,4 +1,8 @@
-function cart() {
-  return [];
+export default function cart(state, action) {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return [...state, action.product];
+    default:
+      return state || [];
+  }
 }
-export default cart();
