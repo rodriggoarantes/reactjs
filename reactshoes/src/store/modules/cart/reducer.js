43fbf526c98b909db85158dc55ec9138d9e953f7
rodrigo <rodrigo.arantes@totvs.com.br>
2019-11-11 13:58:57 -0300

    Redux Saga toasty de erros

--- reactshoes/src/store/modules/cart/reducer.js
+++ reactshoes/src/store/modules/cart/reducer.js
@@ -5,13 +5,8 @@ export default function cart(state, action) {
   switch (action.type) {
     case types.ADD_SUCCESS:
       return produce(state, draft => {
-        const productIndex = draft.findIndex(p => p.id === action.product.id);
-
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({ ...action.product, amount: 1 });
-        }
+        const { product } = action;
+        draft.push(product);
       });
     case types.REMOVE:
       return produce(state, draft => {
--- reactshoes/src/store/modules/cart/sagas.js
+++ reactshoes/src/store/modules/cart/sagas.js
@@ -1,11 +1,39 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
-import { types, addToCartSuccess } from './actions';
+import { types, addToCartSuccess, updateAmount } from './actions';
 import api from '../../../services/api';
 
+import { formatPrice } from '../../../util/format';
+
 function* addtoCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
-  yield put(addToCartSuccess(response.data));
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+  if (amount > stockAmount) {
+    console.tron.warn('ERRO');
+    return;
+  }
+
+  if (productExists) {
+    const ammount = productExists.amount + 1;
+    yield put(updateAmount(id, ammount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest(types.ADD, addtoCart)]);
