53197ca41a1e70d67ac2654768c72a83aaa53b46
rodrigo <rodrigo.arantes@totvs.com.br>
2019-11-12 18:52:40 -0300

    #reacthooks useState e useEffect

--- hello-hooks/src/pages/Main/index.js
+++ hello-hooks/src/pages/Main/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import Container from '../../components/Container';
+import React, { useState, useEffect } from 'react';
 
 function Main() {
+  const [techs, setTech] = useState(['React', 'Native', 'Node']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    if (newTech && newTech !== '') {
+      setTech([...techs, newTech]);
+      setNewTech('');
+    }
+  }
+
+  useEffect(() => {
+    const techStorage = localStorage.getItem('techs');
+    if (techStorage) {
+      setTech(JSON.parse(techStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
   return (
-    <Container>
-      <h1>Hello World</h1>
-    </Container>
+    <>
+      <ul>
+        {techs.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        onChange={e => setNewTech(e.target.value)}
+        value={newTech}
+      />
+      <button onClick={handleAdd}>Adicionar</button>
+    </>
   );
 }
 
