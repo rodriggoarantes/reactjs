5b8cfb2c355c0d3260c9a390916a2677576ed608
rodrigo <rodrigo.arantes@totvs.com.br>
2020-01-13 10:07:38 -0300

    Criando estrutura para FindDev

---
+++ findev/README.md
@@ -0,0 +1,3 @@
+# find-dev / findev
+
+OminiStack 10 FINDEV - encontre devs proximos a voce.
---
+++ findev/src/pages/Main/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+
+function Main() {
+  const [techs, setTech] = useState(['React', 'Native', 'Node']);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    if (newTech && newTech !== '') {
+      setTech([...techs, newTech]);
+      setNewTech('');
+    }
+  }, [techs, newTech]);
+
+  useEffect(() => {
+    const techStorage = localStorage.getItem('techs');
+    if (techStorage) {
+      setTech(JSON.parse(techStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  const techsSize = useMemo(() => techs.length, [techs]);
+
+  return (
+    <>
+      <ul>
+        {techs.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <br />
+      <strong>Você tem {techsSize} tecnologia(s)</strong>
+      <br />
+      <br />
+      <input
+        type="text"
+        onChange={e => setNewTech(e.target.value)}
+        value={newTech}
+      />
+      <button onClick={handleAdd}>Adicionar</button>
+    </>
+  );
+}
+
+export default Main;
