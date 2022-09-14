8f79ec18fe85fd48612cabef5bf7017002a38470
Rodrigo Arantes <rodriggoarantes@gmail.com>
2022-09-14 15:32:32 -0300

    #reactjs #esports hello

--- esports-duo/src/App.tsx
+++ esports-duo/src/App.tsx
@@ -1,16 +1,35 @@
 import { useState } from 'react'
 
+interface ButtonProps {
+  title: String;
+  count: number;
+  callback: Function
+}
+
+function Button(props: ButtonProps) {
+  const { count, callback, title } = props;
+
+  const handleClick = () => callback((count: any) => count + 1);
+
+  return (
+    <button onClick={handleClick}>
+      {title} | {count}
+    </button>
+  )
+}
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <hr/>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button title="Increment+" count={count} callback={setCount} />
       </div>
+      <h3>Valor Atual: {count}</h3>
     </div>
   )
 }
