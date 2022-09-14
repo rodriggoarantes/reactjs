e45011c2685b4f8e9f7138cbfcfb2d62be6dca08
Rodrigo Arantes <rodriggoarantes@gmail.com>
2022-09-14 15:13:52 -0300

    #reactjs #esports projeto inicial com vite

---
+++ esports-duo/src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+const root : HTMLElement = document.getElementById('root') as HTMLElement;
+
+ReactDOM
+  .createRoot(root)
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
